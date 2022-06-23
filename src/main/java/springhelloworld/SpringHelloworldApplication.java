d26196804e8aa508ce7343617c222c1936872c4a
Matheus Leal <matheuslealpa@gmail.com>
2022-06-22 23:31:39 -0300

    Hello world with springboot

---
+++ src/main/java/springhelloworld/SpringHelloworldApplication.java
@@ -0,0 +1,13 @@
+package springhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/main/java/springhelloworld/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package springhelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Hello Word Project for SpringBoot";
+    }
+}
